d79579dc9dbec8dbf93dff1ce476639ad3376e4e
Jan Moringen <jan.moringen@posteo.de>
2024-12-20 10:49:28 +0100

    Export names of readers for WORD subclasses

--- code/package.lisp
+++ code/package.lisp
@@ -6,6 +6,57 @@
    #:case
    #:number)
 
+  ;; Word protocol
+  (:export
+   #:base)
+
+  ;; Noun protocol
+  (:export
+   #:number
+   #:case
+   #:gender
+   #:singular)
+
+  ;; Verb protocol
+  (:export
+   #:person
+   #:tense
+   #:mood
+   #:negative
+   #:contraction
+   #:strength
+   #:infinitive)
+
+  ;; Adjective protocol
+  (:export
+   #:degree)
+
+  ;; Pronoun protocol
+  (:export
+   #:person
+   #:number
+   #:gender
+   #:case)
+
+  ;; Possessive pronoun protocol
+  (:export
+   #:refnumber)
+
+  ;; Determiner protocol
+  (:export
+   #:number)
+
+  ;; Article protocol
+  (:export
+   #:number
+   #:determinate)
+
+  ;; Possessive adjective protocol
+  (:export
+   #:gender
+   #:person
+   #:refnumber)
+
   (:export
    #:english-lookup
    #:english-check-paragraph))
