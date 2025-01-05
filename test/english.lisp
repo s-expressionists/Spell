7807dffb2da0b56e7f8d0bd87899e3d185229e2c
Jan Moringen <jan.moringen@posteo.de>
2025-01-05 16:56:33 +0100

    test: Fix another typo

--- test/english.lisp
+++ test/english.lisp
@@ -27,6 +27,6 @@
           (some (lambda (result)
                   (result-matches-p result expected-class base initargs))
                 results)
-          "~@<For ~S ~S [base ~S]~@[ initiargs ~{~S~^ ~}~], none of the ~
+          "~@<For ~S ~S [base ~S]~@[ initargs ~{~S~^ ~}~], none of the ~
            results ~{~A~^, ~} matches the expected properties.~@:>"
           type word base initargs results))))))
