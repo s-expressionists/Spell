52d9241f9d862ef9df4ba433328d65dabd586ec1
Jan Moringen <jan.moringen@posteo.de>
2025-01-27 19:29:06 +0100

    for documentation generation

--- tools/generate-documentation.lisp
+++ tools/generate-documentation.lisp
@@ -20,6 +20,13 @@
               (#\@ (write-string "@@" stream))
               (t   (write-char character stream)))))
 
+(defun emit-reference (namespace name stream)
+  (let ((type (ecase namespace
+                (:class            "Class")
+                (:generic-function "Generic-Function"))))
+    (format stream "@ref{~A ~(~A~)|~(~A~),~:*~(~A~)}"
+            type (package-name (symbol-package name)) (symbol-name name))))
+
 ;;; Word class descriptions
 
 (defun relevant-direct-subclasses (class)
@@ -52,10 +59,7 @@
      (utilities.print-tree:make-node-printer
       (lambda (stream depth node)
         (declare (ignore depth))
-        (let ((name (class-name node)))
-          (format stream "~S" name)
-                                        ; (format stream "@ref{class-~(~A~),~:*~(~S~)}" name)
-          ))
+        (emit-reference :class (class-name node) stream))
       nil
       #'relevant-direct-subclasses)))
   (format stream "~&@end example~%"))
@@ -64,10 +68,10 @@
                                           &key reference?)
   (let ((name (sb-mop:generic-function-name generic-function)))
     (if reference?
-        (format stream "~2%The generic function ~
-                           @ref{Generic-Function ~(~A~)|~(~A~),~:*~(~A~)} ~
-                           can be applied to words of this class.~%"
-                (package-name (symbol-package name)) (symbol-name name))
+        (progn
+          (format stream "~2%The generic function ")
+          (emit-reference :generic-function name stream)
+          (format stream " can be applied to words of this class.~%"))
         (let ((documentation (documentation generic-function t)))
           (format stream "~2%@defgena{~(~A~),~(~A~)} (~{~(~A~)~^ ~})~%"
                   (symbol-name name) (package-name (symbol-package name))
@@ -77,44 +81,63 @@
               (format stream "@emph{not documented}"))
           (format stream "~%@end deffn~%")))))
 
-(defun find-example-words (class)
-  (format *trace-output* "Class ~A~%" class)
-  (loop :with result = '()
-        :with i = 0
-        :for threshold :from 5 :to 9
-        :do (format *trace-output* "  Threshold ~D~%" threshold)
-        :do (spell::map-similar
-             (lambda (word spelling distance)
-               (declare (ignore distance))
-               (when (typep word class)
-                 (when (and (member class
-                                    (sb-mop:class-direct-superclasses (class-of word))
-                                    :test #'eq)
-                            (not (find #\' spelling))
-                            (or (not (member class (list (find-class 'spell::noun)
-                                                         (find-class 'spell::verb))))
-                                (and (> (length spelling) 5)
-                                     (zerop (mod (incf i) 7)))))
-                   (push (cons spelling word) result))))
-             "a" spell::*english-dictionary* threshold)
-        :when (>= (length result) 3)
-          :do (return (subseq result 0 3))
-        :finally (return (subseq result 0 (min (length result) 3)))))
+(defun find-example-words ()
+  (let ((result (make-hash-table :test #'eq))
+        (i      0))
+    (block nil
+      (spell:map-entries
+       (lambda (spelling word)
+         (when (typep word 'spell::personal-pronoun) (break))
+         (let* ((class    (second (sb-mop:class-direct-superclasses
+                                   (class-of word))))
+                (existing (gethash class result '())))
+           (when (or (null existing)
+                     (and (not (find #\' spelling))
+                          (> (length spelling) 5)
+                          (or (< (length existing) 10)
+                              (zerop (mod (incf i) 377)))))
+            (setf (gethash class result '())
+                  (list* (cons spelling word) existing))))
+         #+no (when (typep word class)
+           (when (and (member class
+                              (sb-mop:class-direct-superclasses (class-of word))
+                              :test #'eq)
+                      (not (find #\' spelling))
+                      (or (not (member class (list (find-class 'spell::noun)
+                                                   (find-class 'spell::verb))))
+                          (and (> (length spelling) 5)
+                               (zerop (mod (incf i) 7)))))
+             (push (cons spelling word) result)
+             (when (> (length result) 3)
+               (return)))))
+       spell::*english-dictionary*))
+    ; (subseq result 0 (min (length result) 3))
+    result))
 
-(defvar *examples*
-  (mapcar #'find-example-words (remove (find-class 'spell::word) (all-word-classes))))
+(defvar *examples* (find-example-words)
+  ; (mapcar #'find-example-words (remove (find-class 'spell::word) (all-word-classes)))
+  )
 
 (defun emit-word-class-description (class stream seen-generic-functions)
   (let* ((superclasses     (sb-mop:class-direct-superclasses class))
          (name             (class-name class))
          (superclass-names (mapcar #'class-name superclasses))
          (documentation    (documentation class t)))
-    (format stream "~2%@defclassa{~(~A~),~(~A~)} (~{~A~^ ~})~% "
-            (symbol-name name) (package-name (symbol-package name)) superclass-names)
+    (let ((*standard-output* stream))
+      (emit-section (format nil "Word Class ~(~A~)" name) :level "subsection"))
+    (format stream "~2%@defclassa{~(~A~),~(~A~)} ("
+            (symbol-name name) (package-name (symbol-package name)))
+    (loop :for superclass :in (set-difference
+                               superclass-names
+                               '(utilities.print-items:print-items-mixin))
+          :for first? = t :then nil
+          :unless first?
+            :do (format stream ", ")
+          :do (emit-reference :class superclass stream))
+    (format stream ")~%")
     (if documentation
         (emit-string documentation stream)
         (format stream "@emph{not documented}~%"))
-
     (format stream "@end deftp~%")
 
     (mapc (lambda (generic-function)
@@ -123,7 +146,19 @@
                                   generic-function seen-generic-functions t))))
               (emit-generic-function-description
                generic-function stream :reference? reference?)))
-          (relevant-generic-functions class))))
+          (relevant-generic-functions class))
+
+
+    (let ((class-examples (gethash class *examples*)))
+      (unless (null class-examples)
+        (format stream "~2%Examples~@
+                    @multitable @columnfractions .2 .7~%")
+        (loop :repeat 3
+              :for (spelling . word) :in class-examples
+              :do (format stream "@item ~A~%@tab @t{" spelling)
+                  (emit-string (princ-to-string word) stream)
+                  (format stream "}~%" ))
+        (format stream "@end multitable~%")))))
 
 (defun emit-word-class-descriptions ()
   (a:with-output-to-file (stream "generated-word-classes.texi"
